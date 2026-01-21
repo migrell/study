893324b9f33b7962b848d4a9a2b63357cf210b78
migrell <parkjiho333@naver.com>
2026-01-21 22:20:04 +0900

    first commit

---
+++ project_1/project_1.srcs/sources_1/new/full_adder.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output s,
+    output c
+);
+
+    wire w_s;
+    wire w_c1, w_c2;
+
+    assign c = w_c1 | w_c2;
+
+    half_adder U_HA1(
+        .a(a),
+        .b(b),
+        .s(w_s),
+        .c(w_c1)
+);
+
+    half_adder U_HA2(
+        .a(w_s),
+        .b(cin),
+        .s(s),
+        .c(w_c2)
+    );
+
+
+
+endmodule
